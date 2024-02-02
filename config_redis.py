f17de14e9069f33431b425da3fe432ae028a4906
Ken Kranz <ken.kranz57@gmail.com>
2024-02-01 21:43:32 -0500

    update files for linode

--- config_redis.py
+++ config_redis.py
@@ -1,13 +1,4 @@
 import redis
-import os
-from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, "local.env"))
-
-redis_hostname = os.getenv("AZURE_REDIS_HOSTNAME")
-redis_password = os.getenv("AZURE_REDIS_PASSWORD")
-azure_redis = redis.StrictRedis(
-    host=redis_hostname, port=6380, db=0, password=redis_password, ssl=True
-)
+azure_redis = redis.StrictRedis(host="localhost", port=6379, decode_responses=False)
 print(azure_redis)
