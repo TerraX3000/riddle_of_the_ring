fec6462386a1b1f97862c187f1e90f45d4ebdf62
Ken Kranz <ken.kranz57@gmail.com>
2024-02-18 20:10:11 -0500

    add player name to start game page

--- stlib/start_or_join_game.py
+++ stlib/start_or_join_game.py
@@ -23,7 +23,7 @@ def get_code_options(id: str = None, excluded_codes: List = None):
     return codes
 
 
-def initialize_game(game_code, player_code, character):
+def initialize_game(game_code, player_code, character, player_name):
     if "is_initialized" not in st.session_state:
         print("+++++ Initializing session +++++++")
         st.session_state["is_initialized"] = True
@@ -32,7 +32,7 @@ def initialize_game(game_code, player_code, character):
         players = {}
         player = {
             "id": 0,
-            "name": "P1",
+            "name": player_name,
             "character": character,
             "current_turn": False,
             "cards": [],
@@ -86,14 +86,14 @@ def initialize_game(game_code, player_code, character):
         return
 
 
-def initialize_player(game_code, player_code, character):
+def initialize_player(game_code, player_code, character, player_name):
     st.session_state["is_initialized"] = True
     st.session_state["points"] = []
     players = get_data("players", game_code=game_code)
 
     player = {
         "id": len(players),
-        "name": "P1",
+        "name": player_name,
         "character": character,
         "current_turn": False,
         "cards": [],
@@ -110,100 +110,113 @@ def run():
     st.query_params.page = "start_or_join_game"
     sid = st.session_state.sid
     navbar.run()
+    st.header("Enter Player Name")
+    name_column, unused = st.columns([2, 10])
+    with name_column:
+        player_name = st.text_input("Enter Player Name", label_visibility="collapsed")
+    if player_name:
+        join_game, start_new_game = st.tabs(["Join with Game Code", "Start New Game"])
+        with join_game:
+            st.header("Join with Game Code")
+            code_column, unused = st.columns([2, 10])
+            with code_column:
+                game_code = st.text_input("Enter Game Code").lower()
 
-    start_new_game, join_game = st.tabs(["Start New Game", "Join with Game Code"])
+                if game_code:
+                    choose_from_updated_player_list = False
+                    games = get_data("games")
+                    valid_game_codes = [game["game_code"] for game in games]
+                    if game_code in valid_game_codes:
+                        current_players = get_data("players", game_code=game_code)
+                        current_characters = [
+                            player["character"] for player in current_players.values()
+                        ]
 
-    with start_new_game:
-        st.header("Start New Game")
-        characters = get_data("characters").keys()
-        selected_character = st.radio(
-            "Choose Your Character", options=characters, key="start_as_character"
-        )
-        st.write("Share the Game Code with your fellow players so they can join")
-        current_games = get_data("games")
-        current_game_codes = [game["game_code"] for game in current_games]
-        game_code = st.radio(
-            "Choose Game Code",
-            options=get_code_options(
-                id=f"{sid}-game", excluded_codes=current_game_codes
-            ),
-        )
-        st.write(
-            "Use the Player Code to resume the game as your character if you reload the site"
-        )
-        player_code = st.radio(
-            "Choose Player Code",
-            options=get_code_options(id=f"{sid}-start_player"),
-            key="start_player_code",
-        )
-
-        if st.button("Begin the Quest"):
-            st.query_params.page = "play"
-            st.query_params.game = game_code
-            st.query_params.player = player_code
-            initialize_game(game_code, player_code, selected_character)
-            st.rerun()
-
-    with join_game:
-        st.header("Join with Game Code")
-        code_column, unused = st.columns([2, 10])
-        with code_column:
-            game_code = st.text_input("Enter Game Code").lower()
-
-            if game_code:
-                choose_from_updated_player_list = False
-                games = get_data("games")
-                valid_game_codes = [game["game_code"] for game in games]
-                if game_code in valid_game_codes:
-                    current_players = get_data("players", game_code=game_code)
-                    current_characters = [
-                        player["character"] for player in current_players.values()
-                    ]
-
-                    characters = list(get_data("characters").keys())
-                    available_characters = [
-                        character
-                        for character in characters
-                        if character not in current_characters
-                    ]
-                    if "available_characters" not in st.session_state:
-                        st.session_state["available_characters"] = available_characters
-
-                    placeholder = st.empty()
-                    selected_character = st.radio(
-                        "Choose Your Character",
-                        options=available_characters,
-                        key="join_as_character",
-                    )
-
-                    if available_characters != st.session_state["available_characters"]:
-                        st.session_state["available_characters"] = available_characters
-                        placeholder.info(
-                            f"Please choose your player from the updated list of available players."
-                        )
-                        choose_from_updated_player_list = True
-
-                    st.write(
-                        "Use the Player Code to resume the game as your character if you reload the site"
-                    )
-                    current_player_codes = list(current_players.keys())
-
-                    player_code = st.radio(
-                        "Choose Player Code",
-                        options=get_code_options(
-                            id=f"{sid}-join_player", excluded_codes=current_player_codes
-                        ),
-                        key="join_player_code",
-                    )
-                    if st.button("Join the Quest"):
-                        if not choose_from_updated_player_list:
-                            del st.session_state["available_characters"]
-                            st.query_params.page = "play"
-                            st.query_params.game = game_code
-                            st.query_params.player = player_code
-                            initialize_player(
-                                game_code, player_code, selected_character
+                        characters = list(get_data("characters").keys())
+                        available_characters = [
+                            character
+                            for character in characters
+                            if character not in current_characters
+                        ]
+                        if "available_characters" not in st.session_state:
+                            st.session_state["available_characters"] = (
+                                available_characters
                             )
-                            st.rerun()
-                else:
-                    st.error("That's not a valid game code")
+
+                        placeholder = st.empty()
+                        selected_character = st.radio(
+                            "Choose Your Character",
+                            options=available_characters,
+                            key="join_as_character",
+                        )
+
+                        if (
+                            available_characters
+                            != st.session_state["available_characters"]
+                        ):
+                            st.session_state["available_characters"] = (
+                                available_characters
+                            )
+                            placeholder.info(
+                                f"Please choose your player from the updated list of available players."
+                            )
+                            choose_from_updated_player_list = True
+
+                        st.write(
+                            "Use the Player Code to resume the game as your character if you reload the site"
+                        )
+                        current_player_codes = list(current_players.keys())
+
+                        player_code = st.radio(
+                            "Choose Player Code",
+                            options=get_code_options(
+                                id=f"{sid}-join_player",
+                                excluded_codes=current_player_codes,
+                            ),
+                            key="join_player_code",
+                        )
+                        if st.button("Join the Quest"):
+                            if not choose_from_updated_player_list:
+                                del st.session_state["available_characters"]
+                                st.query_params.page = "play"
+                                st.query_params.game = game_code
+                                st.query_params.player = player_code
+                                initialize_player(
+                                    game_code,
+                                    player_code,
+                                    selected_character,
+                                    player_name,
+                                )
+                                st.rerun()
+                    else:
+                        st.error("That's not a valid game code")
+        with start_new_game:
+            st.header("Start New Game")
+            characters = get_data("characters").keys()
+            selected_character = st.radio(
+                "Choose Your Character", options=characters, key="start_as_character"
+            )
+            st.write("Share the Game Code with your fellow players so they can join")
+            current_games = get_data("games")
+            current_game_codes = [game["game_code"] for game in current_games]
+            game_code = st.radio(
+                "Choose Game Code",
+                options=get_code_options(
+                    id=f"{sid}-game", excluded_codes=current_game_codes
+                ),
+            )
+            st.write(
+                "Use the Player Code to resume the game as your character if you reload the site"
+            )
+            player_code = st.radio(
+                "Choose Player Code",
+                options=get_code_options(id=f"{sid}-start_player"),
+                key="start_player_code",
+            )
+
+            if st.button("Begin the Quest"):
+                st.query_params.page = "play"
+                st.query_params.game = game_code
+                st.query_params.player = player_code
+                initialize_game(game_code, player_code, selected_character, player_name)
+                st.rerun()
